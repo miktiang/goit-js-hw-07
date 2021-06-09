0f60810af78824e12f7909920cc4d07be6de5172
Vitalik Korobchuk <Vitalikkorobchuk@gmail.com>
2021-06-09 17:01:43 +0200

    changed task-06

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,8 @@ const inputToValidate = document.getElementById('validation-input')
 
 function checkInput(text) {
     if (text.currentTarget.value.length === + inputToValidate.dataset.length) {
-        inputToValidate.classList = 'invalid';
-    }else inputToValidate.classList = 'valid';
+        inputToValidate.classList = 'valid';
+    }else inputToValidate.classList = 'invalid';
 }
 
 inputToValidate.addEventListener('blur', checkInput);
